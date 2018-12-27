a12d3203d24f5f358631d2bf26f160eb01485b34
connerv42 <connerverret42@gmail.com>
2018-12-27 14:58:12 -0800

    finished functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const TodoItem = props => (
+  <li>{props.text}</li>
+);
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      newTodo: ''
+    };
+    // binds "this" in jsx render to App component
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const todos = [...this.state.todos, this.state.newTodo];
+    this.setState({
+      todos: todos,
+      newTodo: ''
+    });
+  }
+
   render() {
+    const newTodo = this.state.newTodo;
+    const todos = this.state.todos.map((t, i) => (
+      <TodoItem key={i} text={t} />
+    ));
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form onSubmit={this.handleSubmit}>
+          <p>Todo List</p>
+          <input
+            autoComplete="off"
+            type="text"
+            name="newTodo"
+            value={newTodo}
+            onChange={(e) => this.setState({ [e.target.name]: e.target.value })}
+          />
+          <button type="submit">Add Todo</button>
+        </form>
+
+        <ul>
+          {todos}
+        </ul>
       </div>
     );
   }
